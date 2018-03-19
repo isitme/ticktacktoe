9d3a1ae75b953ff9bf812f21d8558b693a9ce10e
Rakibul <moumita00705@gmail.com>
2018-03-19 19:19:38 +0600

    random is complite

--- js/app.js
+++ js/app.js
@@ -11,145 +11,79 @@ var btnName = {
     i: 9
 };
 
-var usedButton = [];
+var globalUseButton = [];
+var userButton = [1];
+var comButton = [];
 
 // It's the first turn, The random
 function firstTurn (){
+
     var c = Math.floor(Math.random()*9)+1;
-    
-    switch(c){
-        case 1: if(usedButton.includes(1) === true){
-            console.log("False");
+    if(globalUseButton.length === 1){
+        //This is the first step of the Game..
+        if(globalUseButton.includes(c)=== true){
             firstTurn();
-            console.log(c);
-              
         }
         else{
-            
             console.log(c);
-            usedButton.push(c);
-            document.querySelector(".btn1").textContent = "O";
-            document.querySelector(".btn1").disabled = true;
-        }
-        break;
-        case 2: if(usedButton.includes(2) === true){
-            console.log("False");
-            firstTurn();
-            console.log(c);
-              
-        }
-        else{
-            
-            console.log(c);
-            usedButton.push(c);
-            document.querySelector(".btn2").textContent = "O";
-            document.querySelector(".btn2").disabled = true;
-        }
-        break;
-        case 3: if(usedButton.includes(3) === true){
-            console.log("False");
-            firstTurn();
-            console.log(c);
-              
-        }
-        else{
-            
-            console.log(c);
-            usedButton.push(c);
-            document.querySelector(".btn3").textContent = "O";
-            document.querySelector(".btn3").disabled = true;
-        }
-        break;
-        case 4: if(usedButton.includes(4) === true){
-            console.log("False");
-            firstTurn();
-            console.log(c);
-              
-        }
-        else{
-            
-            console.log(c);
-            usedButton.push(c);
-            document.querySelector(".btn4").textContent = "O";
-            document.querySelector(".btn4").disabled = true;
-        }
-        break;
-        case 5: if(usedButton.includes(5) === true){
-            console.log("False");
-            firstTurn();
-            console.log(c);
-              
-        }
-        else{
-            
-            console.log(c);
-            usedButton.push(c);
-            document.querySelector(".btn5").textContent = "O";
-            document.querySelector(".btn5").disabled = true;
-        }
-        break;
-        case 6: if(usedButton.includes(6) === true){
-            console.log("False");
-            firstTurn();
-            console.log(c);
-              
-        }
-        else{
-            
-            console.log(c);
-            usedButton.push(c);
-            document.querySelector(".btn6").textContent = "O";
-            document.querySelector(".btn6").disabled = true;
-        }
-        break;
-        case 7: if(usedButton.includes(7) === true){
-            console.log("False");
-            firstTurn();
-            console.log(c);
-              
-        }
-        else{
-            
-            console.log(c);
-            usedButton.push(c);
-            document.querySelector(".btn7").textContent = "O";
-            document.querySelector(".btn7").disabled = true;
-        }
-        break;
-        case 8: if(usedButton.includes(8) === true){
-            console.log("False");
-            firstTurn();
-            console.log(c);
-              
-        }
-        else{
-            
-            console.log(c);
-            usedButton.push(c);
-            document.querySelector(".btn8").textContent = "O";
-            document.querySelector(".btn8").disabled = true;
-        }
-        break;
-        case 9: if(usedButton.includes(9) === true){
-            console.log("False");
-            firstTurn();
-            console.log(c);
-              
-        }
-        else{
-            
-            console.log(c);
-            usedButton.push(c);
-            document.querySelector(".btn9").textContent = "O";
-            document.querySelector(".btn9").disabled = true;
-        }
-        break;
+            globalUseButton.push(c);
+            comButton.push(c);
+            switch(c){
+                case 1:{
+                    document.querySelector(".btn1").textContent = "O";
+                    document.querySelector(".btn1").disabled = true;
+                }
+                break;
+                case 2:{
+                    document.querySelector(".btn2").textContent = "O";
+                    document.querySelector(".btn2").disabled = true;
+                }
+                break;
+                case 3:{
+                    document.querySelector(".btn3").textContent = "O";
+                    document.querySelector(".btn3").disabled = true;
+                }
+                break;
+                case 4:{
+                    document.querySelector(".btn4").textContent = "O";
+                    document.querySelector(".btn4").disabled = true;
+                }
+                break;
+                case 5:{
+                    document.querySelector(".btn5").textContent = "O";
+                    document.querySelector(".btn5").disabled = true;
+                }
+                break;
+                case 6:{
+                    document.querySelector(".btn6").textContent = "O";
+                    document.querySelector(".btn6").disabled = true;
+                }
+                break;
+                case 7:{
+                    document.querySelector(".btn7").textContent = "O";
+                    document.querySelector(".btn7").disabled = true;
+                }
+                break;
+                case 8:{
+                    document.querySelector(".btn8").textContent = "O";
+                    document.querySelector(".btn8").disabled = true;
+                }
+                break;
+                case 9:{
+                    document.querySelector(".btn9").textContent = "O";
+                    document.querySelector(".btn9").disabled = true;
+                }
+                break;
+            }   
+        }   
     }
-    // Slect the other button autometicly
-    
-
-
-    console.log(c)
+    else{
+        console.log("Not Empty");
+        //This is for 1
+        
+        
+    }
+   
     
 }
 
@@ -158,63 +92,72 @@ function firstTurn (){
 //If user Click the button
 document.querySelector(".btn1").addEventListener("click", function(){
     document.querySelector(".btn1").textContent = "x";
-    usedButton.unshift(1);
-    //document.querySelector(".btn1").disabled = true;
+    globalUseButton.unshift(1);
+    userButton.push(1);
+    document.querySelector(".btn1").disabled = true;
     firstTurn();
 })
 
 document.querySelector(".btn2").addEventListener("click", function(){
     document.querySelector(".btn2").textContent = "x";
     document.querySelector(".btn2").disabled = true;
-    usedButton.unshift(2);
+    globalUseButton.unshift(2);
+    userButton.push(2);
     firstTurn();
 })
 
 document.querySelector(".btn3").addEventListener("click", function(){
     document.querySelector(".btn3").textContent = "x";
-    usedButton.unshift(3);
+    globalUseButton.unshift(3);
+    userButton.push(3);
     document.querySelector(".btn3").disabled = true;
     firstTurn();
 })
 
 document.querySelector(".btn4").addEventListener("click", function(){
     document.querySelector(".btn4").textContent = "x";
-    usedButton.unshift(4);
+    globalUseButton.unshift(4);
+    userButton.push(4);
     document.querySelector(".btn4").disabled = true;
     firstTurn();
 })
 
 document.querySelector(".btn5").addEventListener("click", function(){
     document.querySelector(".btn5").textContent = "x";
-    usedButton.unshift(5);
+    globalUseButton.unshift(5);
+    userButton.push(5);
     document.querySelector(".btn5").disabled = true;
     firstTurn();
 })
 
 document.querySelector(".btn6").addEventListener("click", function(){
     document.querySelector(".btn6").textContent = "x";
-    usedButton.unshift(6);
+    globalUseButton.unshift(6);
+    userButton.push(6);
     document.querySelector(".btn6").disabled = true;
     firstTurn();
 })
 
 document.querySelector(".btn7").addEventListener("click", function(){
     document.querySelector(".btn7").textContent = "x";
-    usedButton.unshift(7);
+    globalUseButton.unshift(7);
+    userButton.push(7);
     document.querySelector(".btn7").disabled = true;
     firstTurn();
 })
 
 document.querySelector(".btn8").addEventListener("click", function(){
     document.querySelector(".btn8").textContent = "x";
-    usedButton.unshift(8);
+    globalUseButton.unshift(8);
+    userButton.push(8);
     document.querySelector(".btn8").disabled = true;
     firstTurn();
 })
 
 document.querySelector(".btn9").addEventListener("click", function(){
     document.querySelector(".btn9").textContent = "x";
-    usedButton.unshift(9);
+    globalUseButton.unshift(9);
+    userButton.push(9);
     document.querySelector(".btn9").disabled = true;
     firstTurn();
 })
@@ -246,7 +189,7 @@ document.querySelector(".btn9").addEventListener("click", function(){
         
 //     }
 //     else{
-//         console.log("first success");
+//         console.log("first primary");
         
 //         console.log(t.find(checkBtn));
         
